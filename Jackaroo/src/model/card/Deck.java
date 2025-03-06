c56975534c4dd411f994dd92a8ac8242eb74bb69
khalid-hamdy1110 <khalid.hamdy1110@gmail.com>
2025-03-07 00:18:09 +0200

    Removed unnecessary attributes from the BoardManager Interface. Added a function that accounts for quotations when reading CSV.

--- Jackaroo/src/model/card/Deck.java
+++ Jackaroo/src/model/card/Deck.java
@@ -19,51 +19,51 @@ public class Deck {
 		
 		BufferedReader br = null;
 		try {
+			
 			// Initializing the cardsPool array
 			cardsPool = new ArrayList<Card>();
 			
 			br = new BufferedReader(new FileReader(CARDS_FILE)); 
 			String row; // To store the line read from the CSV file
-			while ((row = br.readLine()) != null) { // Reads a line and stores it and checks if it is null
-	            String[] values = row.split(","); // Splitting the values into an array of Strings
-
+			while ((row = br.readLine()) != null) { // Reads a line, stores it and checks if it is null
+	            String[] values = rowSplitter(row); // Splitting the values into an array of Strings
+	            
 	            Card c;
-	            String description = values[3].replaceAll("\"", "");
 	            
 	            // {0 code, 1 frequency, 2 name, 3 description, 4 rank, 5 suit}
 	            switch(values[0]) { // Choosing the type of card
 	            case "1":
-	            	c = new Ace(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new Ace(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 	            case "13":
-	            	c = new King(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new King(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 	            case "12":
-	            	c = new Queen(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new Queen(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 	            case "11":
-	            	c = new Jack(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new Jack(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 				case "10":
-					c = new Ten(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+					c = new Ten(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	        		break;
 				case "7":
-	            	c = new Seven(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new Seven(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 				case "5":
-	            	c = new Five(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new Five(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 				case "4":
-	            	c = new Four(values[2], description, Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new Four(values[2], values[3], Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 				case "14":
-	            	c = new Burner(values[2], description, boardManager, gameManager);
+	            	c = new Burner(values[2], values[3], boardManager, gameManager);
 	            	break;
 				case "15":
-	            	c = new Saver(values[2], description, boardManager, gameManager);
+	            	c = new Saver(values[2], values[3], boardManager, gameManager);
 	            	break;
 	            default:
-	            	c = new Standard(values[2], description, Integer.parseInt(values[4]), Suit.valueOf(values[5]), boardManager, gameManager);
+	            	c = new Standard(values[2], values[3], Integer.parseInt(values[4]), Suit.valueOf(values[5]), boardManager, gameManager);
 	            	break;
 	            }
 	            
@@ -99,5 +99,34 @@ public class Deck {
 		}
 		
 		return newPool;
+	}
+	
+	// Used to account for the cases where the CSV contains <"> and commas inside
+	private static String[] rowSplitter(String row) {
+		ArrayList<String> result = new ArrayList<String>();	
+		String currentItem = "";
+		boolean quotation = false;
+		
+		// Iterates through each character in the row and adds to the String list
+		for (int i = 0; i < row.length(); i++) {
+			if (row.charAt(i) == '"') {
+				quotation = !quotation;
+			} else if (row.charAt(i) == ',' && !quotation) {
+				result.add(currentItem);
+				currentItem = "";
+			} else {
+				currentItem += row.charAt(i);
+			}
+		}
+		
+		result.add(currentItem);
+		
+		// Moving the items in the String list into the string array
+		String[] resultArr = new String[result.size()];
+		for(int i = 0; i < result.size(); i++) {
+			resultArr[i] = result.get(i);
+		}
+		
+		return resultArr;
 	}
 }
